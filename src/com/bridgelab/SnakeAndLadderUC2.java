2ef839d4a32a8866e4602ce578c1c650dc0495c4
123akg <abhijeetkumargiri7070@gmail.com>
2020-09-23 23:16:20 +0530

    Use Case Added

---
+++ src/com/bridgelab/SnakeAndLadderUC2.java
@@ -0,0 +1,10 @@
+package com.bridgelab;
+
+public class SnakeAndLadderUC2 {
+	public static void main(String[ ] args)
+    {
+	int PlayerPosition=0, turn=0;
+	int DieRoll =(int)Math.floor(Math.random()*10)%6 +1;
+}
+
+}
